c370f80a6789610c367b37bfb924678edae8207e
d00406567 <denghong9@huawei.com>
2018-05-01 11:08:52 +0800

    初始化模块

---
+++ macrotools-core/src/main/java/com/macro/it/macrotools/cache/TableDataCache.java
@@ -0,0 +1,12 @@
+package com.macro.it.macrotools.cache;
+
+/**
+ * 
+ * @Description:
+ * @Author Dimmacro,d00406567
+ * @Company HuaWei
+ * @Create time: 2018年5月1日上午11:04:12
+ *
+ */
+public class TableDataCache {
+}
